9de3b0c63c3bdff1d24a0e0278a6593c16fd6108
dajvido <dawid.jaskot@gmail.com>
2015-07-07 22:36:12 +0200

    Configure spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,7 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 RSpec.configure do |config|
   config.include Devise::TestHelpers, type: :controller
   config.include FactoryGirl::Syntax::Methods
+
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
@@ -18,6 +19,13 @@ RSpec.configure do |config|
   # config.mock_with :mocha
   # config.mock_with :flexmock
   # config.mock_with :rr
+  config.mock_with :rspec do |config|
+    config.syntax = [:should, :expect]
+  end
+
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = [:should, :expect]
+  end
 
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
